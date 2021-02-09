93bb5f4702083783c4769f94980cf41a3f19aa76
Caleb Olojo <belac335@gmail.com>
2021-02-09 15:47:11 +0100

    freaking annoying valiadtion script not working as plannned

--- index.js
+++ index.js
@@ -1,35 +1,76 @@
 // targetting all form input fields
-var firstChoice = document.getElementById("firstChoice");
-var secondChoice = document.getElementById("secondChoice");
-var thirdChoice = document.getElementById("thirdChoice");
+var firstChoice = document.getElementById("firstChoice").value;
+var secondChoice = document.getElementById("secondChoice").value;
+var thirdChoice = document.getElementById("thirdChoice").value;
 
 // button element
 var button = document.getElementById("submit-btn");
 
+// targetting the form
+var form = document.getElementById("form");
+
 // error message field: thi field would display all the error
 // messages that would be comming from the
 // validation function
-var err = document.getElementById("errMsg");
+let err = document.getElementById("errMsg");
 
 // validation handler
 function validateInput(e) {
   e.preventDefault();
-  if (!firstChoice.value && !secondChoice.value && !thirdChoice.value) {
-    alert("all input fileds cannot be empty");
-  } else if (
-    firstChoice.value !== "calculus" ||
-    secondChoice.value !== "calculus" ||
-    thirdChoice.value !== "calculus"
-  ) {
-    alert("You choices must include calculus");
-  } else if (firstChoice.value === "") {
+  if (firstChoice === "") {
     alert("first choice cannot be empty");
-  } else if (secondChoice.value === "") {
-    alert("second choice cannot be empty");
-  } else if (thirdChoice.value === "") {
-    alert("your third choice cannot be blank");
+    return;
   }
-  return;
+  if (secondChoice === "") {
+    alert("second choice cannot be empty");
+    return;
+  }
+  if (thirdChoice === "") {
+    alert("your third choice cannot be blank");
+    return;
+  }
+  if (
+    firstChoice !== "calculus" &&
+    secondChoice !== "calculus" &&
+    thirdChoice !== "calculus"
+  ) {
+    alert("your choice should include claculus");
+    return;
+  }
 }
 
 button.addEventListener("click", validateInput);
+
+window.addEventListener("load", () => {
+  // the AJAX http request goes here
+  function postStudentCourses() {
+    // initialize the http request variable
+    const req = new XMLHttpRequest();
+
+    const coursesChoice = new FormData(form);
+
+    // on succesful student courses posting request
+    // get the response text from the server
+    req.addEventListener("load", (e) => {
+      alert(e.target.responseText);
+    });
+    // if the process encounters an error
+    // show a message thorugh the browser's alert
+    req.addEventListener("load", () => {
+      alert("An error ocur while trying to send your courses to the server.");
+    });
+    // this mehod below sets our post request
+    req.open("POST", "https://example.com");
+    req.send(coursesChoice);
+  }
+
+  // attaching an event handler to the form element.
+  // ensures that the inputs are valiadted completely.
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // the function below ensures that inputs are validated before posting.
+    validateInput();
+
+    postStudentCourses();
+  });
+});
